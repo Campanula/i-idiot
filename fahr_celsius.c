74549f10846282e5e35710176b75c25de4c5ae15
chaney <chaneyzorn@gmail.com>
2018-06-25 23:39:39 +0800

    use a compilation database

--- fahr_celsius.c
+++ fahr_celsius.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int fahr_celsius() {
     double fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
+    fahr = LOWER;
+    while (fahr <= UPPER) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
 
 int fahr_for_celsius() {
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr -= 20) {
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
