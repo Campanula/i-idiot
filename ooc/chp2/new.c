49ff277cc48cff426cf3acc2126a8e71bb8b30dd
chaney <chaneyzorn@gmail.com>
2019-01-22 14:46:19 +0800

    implement String

--- ooc/chp2/new.c
+++ ooc/chp2/new.c
@@ -10,6 +10,7 @@ void * new(const void * _class, ...) {
 
     assert(p);
 
+    // 将分配出的内存的开头位置赋值为指向类型描述符的指针
     *(const struct Class **) p = class;
 
     if (class -> ctor) {
@@ -23,11 +24,38 @@ void * new(const void * _class, ...) {
 }
 
 void delete(void * self) {
+    // 取出对象中头部的指向类型描述符的指针
     const struct Class ** cp = self;
 
     if (self && * cp && (* cp) -> dtor) {
         self = (* cp) -> dtor(self);
     }
     free(self);
+}
+
+int differ(const void * self, const void * b) {
+    // 取出对象中头部的指向类型描述符的指针
+    const struct Class * const * cp = self;
+
+    assert(self && * cp && (* cp) -> differ);
+    // 调用类型描述符中指向的方法
+    return (* cp) -> differ(self, b);
+}
+
+void * clone(const void * self) {
+    const struct Class * const * cp = self;
+
+    assert(self && * cp && (* cp) -> clone);
+
+    return (* cp) -> clone(self);
+}
+
+size_t sizeOf(const void * self) {
+    const struct Class * const * cp = self;
+
+    assert(self && * cp);
+
+    // 返回类型描述符中指定的大
+    return (* cp) -> size;
 }
 
